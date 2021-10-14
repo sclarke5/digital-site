42f32eff8d0d8d895d693a22a874c25d27d54680
sclarke5 <sclarke5555@gmail.com>
2021-10-14 12:02:33 -0400

    feat: add more fields to case-study-teaser

--- components/CaseStudyTeaser.vue
+++ components/CaseStudyTeaser.vue
@@ -1,14 +1,33 @@
 <template>
   <nuxt-link
     :to="caseStudyLink"
-    class="case-study-teaser block py-4 px-6 border rounded border-gray-400 my-12">
-    <h2 class="pt-2 pb-4 text-2xl font-bold text-white">
-      {{ caseStudyContent.name }}
-    </h2>
-    <p class="pb-6 leading-relaxed text-blue-200">
-      {{ caseStudyContent.brief }}
-    </p>
-    <img class="w-1/2" :src="caseStudyContent.primary_image.filename" />
+    class="case-study-teaser block py-4 px-6 border rounded border-gray-400">
+    <div class="case-study-container flex">
+      <div class="case-study-left px-6">
+          <h1 class="pt-2 pb-4 text-4xl font-bold text-white">
+            {{ caseStudyContent.name }}
+          </h1>
+          <h2 class="pt-2 pb-4 text-2xl font-bold text-white">
+            {{ caseStudyContent.client }}
+          </h2>
+          <p class="pb-6 leading-relaxed text-blue-200">
+            {{ caseStudyContent.brief }}
+          </p>
+          <p class="pb-6 leading-relaxed text-white">
+            The Challenge: <br />
+            {{ caseStudyContent.challenge }}
+          </p>
+          <p class="pb-6 leading-relaxed text-white">The Approach: <br />
+            {{ caseStudyContent.approach }}
+          </p>
+      </div>
+      <div class="case-study-right">
+        <img :src="caseStudyContent.primary_image.filename" />
+        <p class="pb-6 leading-relaxed text-white">Price Range: {{ caseStudyContent.price_range }}</p>
+        <p class="pb-6 leading-relaxed text-white">Results: {{ caseStudyContent.results }}</p>
+      </div>
+    </div>
+
   </nuxt-link>
 </template>
  
@@ -31,6 +50,9 @@ export default {
   .case-study-teaser {
     transform: rotate(90deg);
     margin-bottom: 200vh;
-    margin-top: -20em;
+    margin-top: -10em;
+  }
+  .case-study-left {
+    width: 50%;
   }
 </style>
